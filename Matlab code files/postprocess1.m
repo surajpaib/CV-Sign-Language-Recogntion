78d2bab005c5324a7bba82fbbc95ff71f2865b17
surajpaib <surajballambat@gmail.com>
2016-02-26 19:09:49 +0530

    blocksegmentation
    The blocks have been segmented and put into a
    4096X16 logical

---
+++ Matlab code files/postprocess1.m
@@ -0,0 +1,29 @@
+I=imread('C:\Users\Suraj\OneDrive\Documents\8th Sem\Repository\Deepthi\Gesture1\variant1.png');
+img=imresize(I,[256,256]);
+
+
+fun = @(block_struct) procblock(block_struct.data);
+B=blockproc(img,[16 16],fun,'UseParallel',true);
+
+k=1;
+for j=1:16
+     for i=1:4096
+         C(k,j)=B(i,j);
+         
+         if (k>256)
+             
+            k=1;
+         else
+             k=k+1;
+             
+         end
+     end
+end
+imshow(C);
+
+
+
+
+
+
+
