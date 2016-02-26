e30542bb82ed33b79daa5af7aeec4114c8de8221
surajpaib <surajballambat@gmail.com>
2016-02-26 19:39:19 +0530

    Added to Excel Sheet
    The added code for saving to excel files

--- Matlab code files/postprocess1.m
+++ Matlab code files/postprocess1.m
@@ -8,15 +8,18 @@ img=imresize(I,[256,256]);
 
 fun = @(block_struct) procblock(block_struct.data);
 B=blockproc(img,[16 16],fun,'UseParallel',true);
-
+C=zeros(256,1);
 k=1;
+count=1;
 for j=1:16
      for i=1:4096
-         C(k,j)=B(i,j);
+         C(k,1)=B(i,j);
          
-         if (k>256)
+         if (k>=256)
              
-            k=1;
+          xlswrite('C:\Users\Suraj\OneDrive\Documents\8th Sem\Repository\Suraj\Gesture1\dataset1.xls',C,count);   
+          k=1;
+          count=count+1;
          else
              k=k+1;
              
