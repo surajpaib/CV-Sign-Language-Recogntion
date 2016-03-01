80ed08ace3cd76d0fb32ef730bd3c07bd64003af
surajpaib <surajballambat@gmail.com>
2016-03-01 20:50:46 +0530

    added final dataset excel file

--- Matlab code files/postprocess1.m
+++ Matlab code files/postprocess1.m
@@ -1,27 +1,33 @@
+
+
+rep=1;
 for rept=1:11
    
-    
-I=imread(['C:\Users\Suraj\OneDrive\Documents\8th Sem\Repository\Pritika\Gesture1\variant',num2str(rept)],'png');
+sheet=['CK','CL','CM','CN','CO','CP','CQ','CR','CS','CT','CU'];
 
-i=imresize(I,[256,256]);
-imshow(i);
+I=imread(['C:\Users\Suraj\OneDrive\Documents\8th Sem\Repository\Deepthi\Gesture3\variant',num2str(rept)],'png');
+
 
 img=imresize(I,[256,256]);
+figure
+imshow(img)
 
 
-fun = @(block_struct) procblock(block_struct.data);
-B=blockproc(img,[32 32],fun,'UseParallel',true);
-C=zeros(1024,1);
+fun = @(block_struct) procfn(block_struct.data);
+D=blockproc(img,[64 64],fun,'UseParallel',true);
+C=zeros(4096,1);
 k=1;
 count=1;
-for j=1:8
-     for i=1:8192
-         C(k,1)=B(i,j);
+for j=1:4
+     for i=1:16384
          
-         if (k>=1024)
+         
+         C(k,1)=D(i,j);
+         
+         if (k==4096)
              
-          xlswrite('C:\Users\Suraj\OneDrive\Documents\8th Sem\Repository\Suraj\Gesture1\datasethello.xls',C,count,'BO1:CU1024');   
-          k=1;
+        xlswrite('C:\Users\Suraj\OneDrive\Documents\8th Sem\Repository\datasethello.xls',C,count,[sheet(rep),sheet(rep+1),'1']);   
+        k=1;
           count=count+1;
          else
              k=k+1;
@@ -31,7 +37,7 @@ for j=1:8
 end
 
 
-
+rep=rep+2;
 end
 
 
--- Matlab code files/procblock.m
+++ Matlab code files/procblock.m
@@ -4,17 +4,19 @@ w=cell2mat(varargin);
  % Convert from cell to double.
 
 
-r=ones(1024,1);
-k=1;
-for i=1:32
-    for j=1:32
-        y=sub2ind([32 32],j,i);
-        r(k)=w(y);
-        k=k+1;
-        
-    
-        
-        
-    end
+r=ones(4096,1);
+% k=1;
+% for i=1:64
+%     for j=1:64
+%         y=sub2ind([64 64],j,i);
+%         r(k)=w(y);
+%         
+%         
+%         k=k+1;
+%         
+%     
+%         
+%         
+%     end
 end
 
